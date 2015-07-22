93b9c18820294332e422b9813242f98c32cd7f4a
Steven Rohr <srohr3792@gmail.com>
2015-07-22 15:15:25 -0400

    Fixed indents
    I can't stand unindented loops and ifs.

--- threadpool_server.cpp
+++ threadpool_server.cpp
@@ -38,26 +38,28 @@ void *one_thread(void *arg) {
 
     cout << "thread " << tid << " created" << endl;
     for (; ;) {
-	sock_len = sizeof(cli_addr);
-	pthread_mutex_lock(&accept_lock);
-	cli_sockfd = accept(serv_sockfd, (struct sockaddr *)&cli_addr, &sock_len);
-	pthread_mutex_unlock(&accept_lock);
+    	sock_len = sizeof(cli_addr);
+    	pthread_mutex_lock(&accept_lock);
+    	cli_sockfd = accept(serv_sockfd, (struct sockaddr *)&cli_addr, &sock_len);
+    	pthread_mutex_unlock(&accept_lock);
 
-	cout << "thread " << tid << ": ";
-	cout << "remote client IP == " << inet_ntoa(cli_addr.sin_addr);
-	cout << ", port == " << ntohs(cli_addr.sin_port) << endl;
+    	cout << "thread " << tid << ": ";
+    	cout << "remote client IP == " << inet_ntoa(cli_addr.sin_addr);
+    	cout << ", port == " << ntohs(cli_addr.sin_port) << endl;
 
-	while ((n = read(cli_sockfd, buf, MAXBUFLEN)) > 0) {
-	    buf[n] = '\0';
-	    cout << buf << endl;
-	    write(cli_sockfd, buf, strlen(buf));
-	}
-	if (n == 0) {
-	    cout << "client closed" << endl;
-	} else {
-	    cout << "something wrong" << endl;
-	}
-	close(cli_sockfd);
+    	while ((n = read(cli_sockfd, buf, MAXBUFLEN)) > 0) {
+    	    buf[n] = '\0';
+    	    cout << buf << endl;
+    	    write(cli_sockfd, buf, strlen(buf));
+
+            
+    	}
+    	if (n == 0) {
+    	    cout << "client closed" << endl;
+    	} else {
+    	    cout << "something wrong" << endl;
+    	}
+    	close(cli_sockfd);
     }
 }
 
@@ -68,8 +70,8 @@ int main(int argc, char *argv[]) {
     int *tid_ptr;
 
     if (argc != 3) {
-	fprintf(stderr, "%s port number_thread\n", argv[0]);
-	exit(1);
+    	fprintf(stderr, "%s port number_thread\n", argv[0]);
+    	exit(1);
     }
 
     port = atoi(argv[1]);
@@ -90,9 +92,9 @@ int main(int argc, char *argv[]) {
 
     int i;
     for (i = 0; i < number_thread; ++i) {
-	tid_ptr = (int *)malloc(sizeof(int));
-	*tid_ptr = i;
-	pthread_create(&tid, NULL, &one_thread, (void *)tid_ptr);
+    	tid_ptr = (int *)malloc(sizeof(int));
+    	*tid_ptr = i;
+    	pthread_create(&tid, NULL, &one_thread, (void *)tid_ptr);
     }
 
     for (;;)
