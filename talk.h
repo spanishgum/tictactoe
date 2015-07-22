3ecbf87b71600a77a3ab2406d2ea11fd166d4f67
spanishgum <aps10d@my.fsu.edu>
2015-07-22 10:19:02 -0400

    created file

---
+++ talk.h
@@ -0,0 +1,22 @@
+/*
+*  COP5570    |    Parallel, Concurrent, Distributed Programming
+*  Asg #4     |    Tic Tac Toe Game Server
+*  Summer C   |    07/24/15
+*
+*     by Adam Stallard, Steven Rohr
+*
+*
+*  This unit of code will be used to interpret input and output.
+*
+*  The server will send a top lvl parser some user input and
+*   the parser will respond with the type of request that was
+*   sent, or possible error msgs.
+*
+*  After recieving the interpretation, the server will call the
+*   appropriate functions which will handle contiguos input
+*   and output until the request is fully satisfied.
+*
+*
+*
+*
+*/
