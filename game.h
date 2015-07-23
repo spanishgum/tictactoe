c4ea9ec003796767741cf370c28548b21ae6cbbf
spanishgum <aps10d@my.fsu.edu>
2015-07-23 19:44:53 -0400

    Update game.h

--- game.h
+++ game.h
@@ -9,7 +9,8 @@
 using namespace std;
 
 static const char icon[2] = { '#', 'O' };
-
+class game;
+vector<game> games;
 
 class game {
 	public:
@@ -19,20 +20,22 @@ class game {
 		bool fin;
 		time_t time_A;
 		time_t time_B;
-		string user_A;
-		string user_B;
+		string player[2];
 		int moves;
+		int turn;
 		vector<string> observing;
+		bool b_w;
 
-		game(int g_id, string u_A, string u_B, time_t t_A = 600, time_t t_B = 600) {
+		game(int g_id, string u_A, string u_B, bool color, time_t t = 600) {
 			id = g_id;
-			time_A = t_A;
-			time_B = t_B;
-			user_A = u_A;
-			user_B = u_B;
+			time_A = time_B = t;
+			player[0] = u_A;
+			player[1] = u_B;
 			moves = 0;
 			for (int i = 0; i < 9; ++i)
 				board[i] = '.';
+			turn = 0;
+			b_w = color;
 		}
 
 		
@@ -40,46 +43,45 @@ class game {
 			char mark;
 			int spot = get_spot(choice);
 			if (spot < 0) return spot;
-			if (u == user_A)
+			if (u == player[0])
 				mark = icon[0];
-			else if (u == user_B)
+			else if (u == player[1])
 				mark = icon[1];
 			else return -3;
 			
+			board[spot] = mark;
+			
 			++moves;
 			
+			if (moves == 8) {
+				fin = true;
+				return 0;
+			}
+			
+			turn = !turn;
 			/* stop timer for player, start timer for other ***********************/
 		}
 		
 		int get_spot(string str) {
 			int spot;
-		
-			if (str[0] < 'A' || str[0] > 'C' || str[1] < '1' || str[1] > '3'){ // double check range
-				// cout << "Command not supported.\n";
+			if (str[0] < 'A' || str[0] > 'C' || str[1] < '1' || str[1] > '3') // double check range
 				return -1;
-			}
-			
 			spot = (str[0] - 'A') * 3; // go to row
 			spot += (str[1] - '1'); // go to col
-			
-			if (board[spot] != '.') { // check if empty
-				// cout << str[0] << str[1] << " is occupied.\n";
+			if (board[spot] != '.') // check if empty
 				return 0;
-			}
 			else return spot; // good value
 		}
 		
 		string print_board() {
 			stringstream ss;
-			
 			/* metadata */
 			ss << "Black:";
-			ss.width(15); ss << right << user_A;
+			ss.width(15); ss << right << player[0];
 			ss << "       White:";
-			ss.width(15); ss << right << user_B << "\n";
+			ss.width(15); ss << right << player[1] << "\n";
 			ss << " Time:    " << time_A << " seconds"
 				<< "        Time:    " << time_B << " seconds\n";
-			
 			/* actual board */
 			ss << "   1  2  3\n";
 			for (int i = 0; i < 9; ++i) {
@@ -140,6 +142,11 @@ class game {
 			return win;
 		}
 
+		
+		void add_observer(string u_name) {
+			stringstream ss;
+			observing.push_back(u_name);
+		}
 };
 
 
