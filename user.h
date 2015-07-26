53fe68e629787145bf7103fa1ecbc74395779d5b
spanishgum <aps10d@my.fsu.edu>
2015-07-26 19:06:43 -0400

    stream overloads finished
    These overloads allow writing and reading from a file stream.
    They assume the stream will be passed from the correct location, or otherwise will return a data corruption message.

--- user.h
+++ user.h
@@ -42,69 +42,116 @@ class user {
 		int clientcounter;
 		int cli_sockfd;
 
+
+
+
+
 		friend bool operator ==(user &l, user &r) {
 			if ((l.name).compare(r.name) == 0)
 				return true;
 			else return false;
 		}
 
+
 		friend ostream& operator <<(ostream &ofs, user u) {
-			ofs << u.name << "\n" << u.passwd << "\n"
-				<< u.info << "\n"	<< u.rating << "\n"
-				<< u.wins << "\n" << u.loses << "\n"
-				<< u.quiet << "\n";
+			// basic data
+			ofs << u.name << "\n" << u.passwd << "\n" << u.info << "\n"
+				<< u.wins << "\n" << u.loses << "\n" << u.quiet << "\n";
+
+			// blocked list
 			for (vector<string>::iterator b = u.blocked.begin();
 				b != u.blocked.end(); ++b)
 				ofs << (*b) << " ";
-			ofs << "\n"; // end of blocked users list
-			ofs << u.inbox.size() << "\n"; // metadata for mail
+			ofs << "\n";
+
+			// mail
+			ofs << u.inbox.size() << "\n"; // metadata
 			for (vector<mail>::iterator i = u.inbox.begin();
 				i != u.inbox.end(); ++i)
 				ofs << (*i);
-			ofs << u.cli_sockfd << "\n"
-				<< "\f"; // form feed to separate users
+
+			// form feed to separate users
+			ofs << "\f...\n";
 
 			return ofs;
 		}
 
+
 		friend istream& operator >>(istream &ifs, user &u) {
 			stringstream form, tmp;
-			string data, sub_data;
-			mail m;
-			char c; unsigned int num_msgs;
-			do {
-				c = ifs.get();
-				form << c;
-			} while (c != '\f'); // grab all the way to form feed
+			string line, data, sub_data;
+			mail m = mail();
+			int num_lines = 0;
+			unsigned int num_msgs;
 
-			grab_data(); u.name = data;
-			grab_data(); u.passwd = data;
-			grab_data(); u.info = data;
-			grab_data(); u.rating = stod(data);
-			grab_data(); u.wins = stoi(data);
-			grab_data(); u.loses = stoi(data);
-            grab_data(); u.quiet = (bool) stoi(data);
-            grab_data(); /* get list of blocked */
-			while (tmp << data) {
-				sub_data = tmp.str(); // copy elem
-				u.blocked.push_back(sub_data); // push elem
-				tmp.str(string()); // clear buff
+			// grab user form
+			do {
+				getline(ifs, line);
+				form << line << "\n"; // \n removed, add it back
+				++num_lines;
+			} while (line.compare("\f...") != 0); // end of user marker
+
+			// double check user meta data quality
+			if (num_lines < 8) {
+				cerr << "Back up data file may be corrupted. . .\n"
+					<< "\tAborting load for curr user object\n";
+				return ifs;
 			}
-			grab_data(); num_msgs = stoi(data);
-      for (unsigned int i = 0; i < num_msgs; ++i) { /* get mail data - msg ends with \v */
+
+			// input basic user data
+			getline(form, data); u.name = data;
+			getline(form, data); u.passwd = data;
+			getline(form, data); u.info = data;
+			getline(form, data); u.wins = _stoi(data);
+			getline(form, data); u.loses = _stoi(data);
+			u.rating = (double)u.wins / (u.wins + u.loses);
+      getline(form, data); u.quiet = (bool) _stoi(data);
+
+			// input blocked list
+      getline(form, data); // line contains blocked users
+			tmp << data; // put line into stream
+			while (getline(tmp, data, ' ')) // tokenize
+				u.blocked.push_back(data);
+
+			// input mail data
+			getline(form, data); num_msgs = _stoi(data);
+      for (unsigned int i = 0; i < num_msgs; ++i) {
 				form >> m;
 				u.inbox.push_back(m);
 			}
-      grab_data(); u.cli_sockfd = stoi(data);
 
-			form >> c; // make sure data was read in properly
-			if (c != '\f') {
-				cout << "Error: user form alignment has been corrupted.\n";
-				exit(-1);
+			// double check mail data quality
+			getline(form, data);
+			if (data.compare("\f...") != 0) {
+				cerr << "Back up data file may be corrupted. . .\n"
+					<< "\tLoad for curr user ended unexpectedly\n";
+				return ifs;
 			}
 
 			return ifs;
 		}
+
+
+
+
+		// wrapper for stoi to prevent aborts in >> overload
+		static int _stoi(string data) {
+			string sub = "";
+			for(char c : data) {
+				if (isdigit(c)) {
+					sub += c;
+				}
+				else if (c == '-') {
+					if (sub == "") {
+						sub += c;
+					}
+					else break;
+				}
+				else break; // only grab leading digits in string
+			}
+			return stoi(sub);
+		}
+
 
 
 		user(string u_name = "", string u_passwd = "") {
@@ -116,6 +163,9 @@ class user {
 			online = false;
 			match = 0;
 		}
+
+
+
 
 		string stats() {
 			stringstream ss;
