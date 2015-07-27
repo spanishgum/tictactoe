fccccf8a0af7ff75d79e4f9afd10f09b44f42a65
spanishgum <aps10d@my.fsu.edu>
2015-07-26 20:33:22 -0400

    Finished archive API
    Saves all data in user.dat
    Creates and removes swap file as necessary
    Keeps a backup file just in case
    Writes to a log file the backup process actions

--- archive.h
+++ archive.h
@@ -9,32 +9,137 @@
 
 #include <fstream>
 #include <iostream>
+#include <time.h>
+#include <cstdio>
 #include "user.h"
 
-#define USER_DAT "users.dat"
+#define USER_DAT "data/users.dat"
+#define USER_SWP "data/users.dat.swap"
+#define SERV_LOG "data/server.log"
+#define SWP_BACK "data/backup.dat"
 
-void save_user(user &u) {
-	ofstream ofs;
-	ofs.open(USER_DAT, ios_base::out);
-	ofs << u;
-	ofs.close();
+void create_swap() {
+	time_t t;
+	struct tm *now;
+	char stamp[80];
+
+	ofstream slog(SERV_LOG, ios_base::out | ios_base::app);
+	t = time(NULL);
+	now = localtime(&t);
+	strftime(stamp, sizeof(stamp), "\n%Y-%m-%d.%X\n", now);
+	slog << stamp << "Begin swap overwrite\n";
+
+	ifstream orig(USER_DAT, ios_base::binary);
+	ofstream swap(USER_SWP, ios_base::binary | ios_base::trunc);
+	swap << orig.rdbuf();
+	orig.close();
+	swap.close();
+
+	t = time(NULL);
+	now = localtime(&t);
+	strftime(stamp, sizeof(stamp), "\n%Y-%m-%d.%X\n", now);
+	slog << stamp << "Swap overwrite complete\n";
+	slog.close();
 }
 
+void create_udat() {
+	time_t t;
+	struct tm *now;
+	char stamp[80];
 
-void load_user(user &u) {
-	ifstream ifs;
-	ifs.open(USER_DAT, ios_base::in);
-	ifs >> u;
+	ofstream slog(SERV_LOG, ios_base::out | ios_base::app);
+	t = time(NULL);
+	now = localtime(&t);
+	strftime(stamp, sizeof(stamp), "\n%Y-%m-%d.%X\n", now);
+	slog << stamp << "Begin udat overwrite\n";
+
+	ofstream udat(USER_DAT, ios_base::out | ios_base::trunc);
+	vector<user>::iterator u;
+	udat << users.size() << "\n"; // metadata
+	for (u = users.begin(); u != users.end(); ++u)
+		udat << (*u);
+	udat.close();
+
+	t = time(NULL);
+	now = localtime(&t);
+	strftime(stamp, sizeof(stamp), "\n%Y-%m-%d.%X\n", now);
+	slog << stamp << "Udat overwrite complete\n";
+	slog.close();
+}
+
+int save_server() {
+	create_swap(); // save orig data in case of crash during overwrite
+	create_udat(); // update curr user data file
+
+	// create back up of swap just in case
+	ifstream swap(USER_DAT, ios_base::binary);
+	ofstream back(USER_SWP, ios_base::binary | ios_base::trunc);
+	back << swap.rdbuf();
+	swap.close();
+	back.close();
+
+	// remove swap
+	if (remove(USER_SWP) != 0)
+		cerr << "Error deleting swap file\n";
+
+	return 0;
+}
+
+void load_users(ifstream &ifs) {
+	user u;
+	int num_users = 0;
+	string line;
+
+	// get metadata
+	getline(ifs, line);
+	for (char c : line) {
+		if (isdigit(c)) {
+			num_users *= 10;
+			num_users += (c - '0');
+		}
+	}
+
+	// clear curr user vector and get users from file
+	users.clear();
+	for (int i = 0; i < num_users; ++i) {
+		ifs >> u;
+		users.push_back(u);
+	}
+
 	ifs.close();
 }
 
+int load_server() {
+	int res;
+	ifstream udat(USER_DAT, ios_base::in);
+	ifstream swap(USER_SWP, ios_base::in);
+	ifstream back(SWP_BACK, ios_base::in);
 
-int save() {
+	if (swap) {
+		cerr << "Server may have crashed unexpectedly on last run\n"
+			<< "\tLoading server from swap file\n";
+		res = 1;
+		load_users(swap);
+	}
+	else if (udat) {
+		res = 0;
+		load_users(udat);
+	}
+	else if (back) {
+		cerr << "Both user data and swap files are missing\n"
+			<< "\tLoading server from backup file\n";
+		res = 2;
+		load_users(back);
+	}
+	else {
+		cerr << "All data and backup files are missing"
+			<< "\tCould not load server\n";
+		res = 3;
+	}
 
-	return 0;
-}
+	udat.close();
+	swap.close();
+	back.close();
 
-int load() {
-
-	return 0;
+	return res;
 }
