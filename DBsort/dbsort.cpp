e05108b140fb66ff0340b910e669d9a63a71f2d3
Steven Rohr <srohr3792@gmail.com>
2015-07-29 21:38:25 -0400

    Version 2
    Now with better (and by that I mean working) printing (uses tmp files,
    it's fine).
    
    Not sure if it'll be very fast yet, but it shouldn't be awful.
    
    make demo0 compiles and runs with test0 (if there)
    make demo1 compiles and runs with test1 (if there).

--- DBsort/dbsort.cpp
+++ DBsort/dbsort.cpp
@@ -24,67 +24,73 @@ long long _l;
 int size = sizeof(_l);
 
 queue<long> inq [NTHREADS]; 		//Each stores 1/NTHREADS total ints, paritioned by range.
-queue<string> outq [NTHREADS]; 		//Each stores 1/NTHREADS total ints, paritioned by range.
+
+ofstream oftmp[NTHREADS];
+ifstream iftmp[NTHREADS];
 
 pthread_mutex_t qlock[NTHREADS];
 
-bool readyToPrint[NTHREADS];
+pthread_cond_t readyToPrint [NTHREADS];
+pthread_mutex_t mymutex[NTHREADS];
+
 bool doneReading = false;
 bool DEBUG = false;
-bool done = false;
-bool donesending[NTHREADS];
+
 
 long long MIN, MAX;
 
 
 static int callback(void *data, int argc, char **argv, char **azColName){   
-   //Get id from data
-   int id = *((int *) argv);
-   while (outq[id].size() > MAXQSIZE){
-   	usleep(250);
-   }
-   
-   pthread_mutex_lock(&qlock[id]);
-   outq[id].push(argv[0]);
-   pthread_mutex_unlock(&qlock[id]);
-   
-   return 0;
+	//Get id from data
+
+	int id = *((int *) data);
+	
+	oftmp[id] << argv[0] << "\n";
+	//cout << "My id = " << id << " Element = " << argv[0] << endl;
+
+	return 0;
 }
 
 
 //Or "Read()". Honestly we're basically doing producer consumer problem.
 void Producer(string path){
+	cout << "Consumer launched!" << endl;
 	int fd = open(path.c_str(), O_RDONLY);
 	long long e;
+	long nNums = 0;
 	while (fd){ //Is this going to be a valid to ensure file okay? Check.
-		if (read(fd, (void*)&e, sizeof(e)) == sizeof(e)){
-			if (NTHREADS > 1){
-				for (int i = 0; i < NTHREADS; i++){
-					if (e <= ((MAX - MIN)/i)){
-						if (DEBUG) printf("Inserted %lld to queue %d\n", e, i);
+		int rd = read(fd, (void*)&e, sizeof(e));
+		if (rd == sizeof(e)){
+			for (int i = 0; i < NTHREADS; i++){
+				if ((i == NTHREADS - 1) || (e <= ((i + 1)*(MAX/(NTHREADS))) + MIN)){
+					nNums++;
+					/*if (nNums%1000 == 0){
+						cout << nNums << ": " << e << endl;
+					}//*/
 
-						while (inq[i].size() > MAXQSIZE) usleep(250);
+					if (DEBUG) 
+						printf("Inserted %lld to queue %d\n", e, i);
 
-						pthread_mutex_lock(&qlock[i]);
-						inq[i].push(e);
-						pthread_mutex_unlock(&qlock[i]);
+					while (inq[i].size() > MAXQSIZE) usleep(250);
 
-						//Element is to be inserted. Onto next.
-						break;
-					}
+					pthread_mutex_lock(&qlock[i]);
+					inq[i].push(e);
+					pthread_mutex_unlock(&qlock[i]);
+
+					//Element is to be inserted. Onto next.
+					break;
 				}
 			}
-			else{
-				while (inq[0].size() > MAXQSIZE) usleep(250);
-
-				pthread_mutex_lock(&qlock[0]);
-				inq[0].push(e);
-				pthread_mutex_unlock(&qlock[0]);
-			}
+		}
+		else if (rd == -1){
+			//EoF
+			break;
 		}
 		else{
 			printf("Err: Read unexpexted number of bytes.\n");
 		}
+
+		//if (nNums > 5000) break;
 	
 	}
 
@@ -99,22 +105,30 @@ void* Consumer(void* arg){
 	char *zErrMsg = 0;
 	int rc;
 	int machineID = id +  3; //3, 4, 5, 6. Reader is 2 or 7.
-	string cmd, query;
+	string cmd, query, fname;
 	stringstream ss;
 
-	pthread_detach(pthread_self());
+	ss << "file" << id << ".tmp";
+	fname = ss.str();
+	ss.str(""); ss.clear();
+
+	oftmp[id].open(fname.c_str());
+
+	long nInserted = 0;
+	cout << "Thread " << id  << " launched!" << endl;
+	
+	/*ss << "ssh -q c5570-" << machineID;
+	cmd = ss.str();
+	ss.str(""); ss.clear();
+	cout << "Thread " << id << " now executing: " << cmd << endl;
+	system(cmd.c_str());//*/
+
+
+	//pthread_detach(pthread_self());
 	string dbname;
 	ss << "db" << id << ".db";
 	dbname = ss.str();
 	ss.str(""); ss.clear();
-
-	readyToPrint[id] = false;
-	
-	ss << "ssh c5570-" << machineID;
-	cmd = ss.str();
-	ss.str(""); ss.clear();
-	cout << "Thread " << id << " now executing: " << cmd << endl;
-	system(cmd.c_str());
 
 	//Make db file
 	sqlite3 * db;
@@ -126,9 +140,10 @@ void* Consumer(void* arg){
 	sqlite3_prepare_v2(db, query.c_str(), query.size(), &createStmt, NULL);
 	if (sqlite3_step(createStmt) != SQLITE_DONE) cout << "ERROR: Could not create table!" << endl;
 
-	while(!doneReading && !inq[id].empty()){
+	while(!(doneReading && inq[id].empty())){
 		if (inq[id].empty()) usleep(250);
 		while (!inq[id].empty()){
+
 			pthread_mutex_lock(&qlock[id]);
 			l = inq[id].front(); //Get next element.
 			inq[id].pop(); //Pop it. We will insert this.
@@ -137,27 +152,33 @@ void* Consumer(void* arg){
 		   ss << "INSERT INTO numbers VALUES (" << l << ", CURRENT_TIMESTAMP);"; 
 		   query = ss.str();
 		   ss.str(""); ss.clear();
+		   //cout << "SQL: " << query << endl;
 
 		   sqlite3_stmt *insertStmt;
 		   sqlite3_prepare(db, query.c_str(), query.size(), &insertStmt, NULL);
 		   if (sqlite3_step(insertStmt) != SQLITE_DONE) cout << "ERROR: Could not enter data into table!" << endl;
-
+		   nInserted++;
+		   if (nInserted%1000 == 0)
+		   		cout << "Thread #" << id << " has inserted " << nInserted << " values." << endl;
 		}
 	}
 
-	donesending[id] = false;
-	readyToPrint[id] = true;
+	cout << "Thread #" << id << " executing SELECT statement." << endl;
+	
 	query = "SELECT ALL value FROM numbers ORDER BY value ASC;";
 	int *arg2 = (int*)malloc(sizeof(*arg2));
     *arg2 = id;
 	rc = sqlite3_exec(db, query.c_str(), callback, arg2, &zErrMsg);
 
-	donesending[id] = true;
-	while(!done) usleep(2500); //Sleep til main is done.
-	//Clear DB
-	cmd = "rm *.db";
-	system(cmd.c_str());
-	exit(0);
+	if (rc != SQLITE_OK){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	    sqlite3_free(zErrMsg);
+	}
+
+	sqlite3_close(db);
+
+	oftmp[id].close();
+	pthread_cond_signal(&readyToPrint[id]);
 } 
 
 int main(int narg, char ** argc){	
@@ -169,17 +190,25 @@ int main(int narg, char ** argc){
 	ofstream ofile;
 	bool firstIteration = true;
 	int code;
+	cmd = "rm *.db";
+	system(cmd.c_str());
+	cmd = "rm *.db-journal";
+	system(cmd.c_str());
 
-	double t0, t1, t2, t3, t4, t5, t6;
+	//double t0, t1, t2, t3, t4, t5, t6;
 
 	if (narg != 2){
 		cout << "Error: Usage: ./dbsort <file to sort> " << endl;
 		exit(1);
 	}
 
+	for (int i = 0; i < NTHREADS; i++){
+		pthread_cond_init(&readyToPrint[i], NULL);
+	}
+
 	path = argc[1];
 
-	t0 = omp_get_wtime();
+	//t0 = omp_get_wtime();
 
 	if (NTHREADS > 1){
 		cout << "Preliminary scan to determine MIN and MAX values...\n";
@@ -187,11 +216,12 @@ int main(int narg, char ** argc){
 
 		while (fd){
 			//Consider skipping ~1000 numbers each iteration. Should result in same ballpark min/max
-			if (read(fd, (void*)&e, sizeof(e)) == sizeof(e)){				
+			int rd = read(fd, (void*)&e, sizeof(e));
+			if (rd == sizeof(e)){				
 				
 				counter++;
-				if (counter%10000 == 0){
-					cout << "Read " << counter << " numbers! MIN = " << MIN << ", MAX = " << MAX << "\n";
+				if (counter%1000 == 0){
+					cout << "Read " << counter << " numbers!\tMIN = " << MIN << ", MAX = " << MAX << "\n";
 				}//*/
 
 				{
@@ -202,22 +232,24 @@ int main(int narg, char ** argc){
 					else if (e < MIN) MIN = e;
 					else if (e > MAX) MAX = e;
 
-					//Skip 10,000 iterations for speed
-					lseek(fd, 10000*sizeof(e), SEEK_CUR);
+					//Skip a shitload of numbers for speed
+					lseek(fd, 100000*sizeof(e), SEEK_CUR);
 				}
 			}
+			else break;
 		}
 
 		close(fd);
-		t2 = omp_get_wtime();
-		cout << "Done. MIN = " << MIN << ", MAX = " << MAX << ". Scan time = " << t1 - t0 << " s.\n"; 
+		//t2 = omp_get_wtime();
+		cout << "Done. MIN = " << MIN << ", MAX = " << MAX << endl; // ". Scan time = " << t1 - t0 << " s.\n"; 
 	}
-	else t2 = omp_get_wtime();
+	//else t2 = omp_get_wtime();
 
 	
 	cout << "Beginning sort routine: Creating processes and dumping data into databases.\n";
 	pthread_t pthreads[NTHREADS];
 	for(int i = 0; i < NTHREADS; i++){
+		cout << "Launching thread " << i << endl;
         int *arg2 = (int*)malloc(sizeof(*arg2));
         *arg2 = i;
         if ((code = pthread_create(&pthreads[i], NULL, Consumer, arg2)) != 0){
@@ -229,47 +261,46 @@ int main(int narg, char ** argc){
 
 	Producer(path);
 
-	t3 = omp_get_wtime();
-	cout << "Done dumping data. (Insert Time = " << t3 - t2 << ", Total time = " 
-		<< t3 - t0 << ")\nMain is now reading back data and printing it to file.";
+	//t3 = omp_get_wtime();
+	cout << "Done dumping data. \nMain is now waiting, then reading back data and printing it to file." << endl;
+	//(Insert Time = " << t3 - t2 << ", Total time = " << t3 - t0 << ")\nMain is now reading back data and printing it to file.";
 
 	ofname = "outfile.txt";
 	ofile.open(ofname.c_str());
 	int tid = 0;
 	long long itr = 0;
-	cout << "Sorted data:\n";
-	while (tid != NTHREADS){
-		while (!donesending[tid] && !outq[tid].empty()){
-			if (outq[tid].empty()) usleep(250);
-			while(!outq[tid].empty()){
-				if (itr%1000 == 0){
-					string s;
-					pthread_mutex_lock(&qlock[tid]);
-					s = outq[tid].front();
-					outq[tid].pop();
-					pthread_mutex_unlock(&qlock[tid]); 
+	long long ele;
+	stringstream ss;
 
-					cout << s << endl;
-					ofile << s << endl;
+	for (int i = 0; i < NTHREADS; i++){
+		pthread_cond_wait(&readyToPrint[i], &mymutex[i]);
+		ss << "file" << i << ".tmp";
+		iftmp[i].open(ss.str().c_str());
+		ss.str(""); ss.clear();
 
-				}
-				else{
-					pthread_mutex_lock(&qlock[tid]);
-					outq[tid].pop();
-					pthread_mutex_unlock(&qlock[tid]); 
-				}
-
-
-				itr++;
+		iftmp[i] >> ele;
+		while (iftmp[i]){
+			if (itr%10 == 0){
+				ofile << ele << endl;
+				cout << ele << endl;
 			}
-		}
-		tid++;
-	}
-	t4 = omp_get_wtime();
-	cout << "Done. (Print Time = " << t4 - t3 << ", Total time = " 
-		<< t4 - t0 << ")\nGoodbye World.";
 
-	done = true;
-	return 0;
+			iftmp[i] >> ele;
+			itr++;
+		}
+		iftmp[i].close();
+	}
+
+
+	
+	//t4 = omp_get_wtime();
+	cout << "Done. \nGoodbye World.\n";
+	cmd = "rm *.db";
+	system(cmd.c_str());
+	cmd = "rm *.db-journal";
+	system(cmd.c_str());
+	cmd = "rm *.tmp";
+	system(cmd.c_str());
+
 
 }
